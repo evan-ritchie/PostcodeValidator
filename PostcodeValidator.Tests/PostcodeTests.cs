6af129708444fb0f6ff449bb2a84ff8a5217e083
evan-ritchie <evan.ritchie@gmail.com>
2017-02-18 13:49:22 +0000

    Updated unit tests

---
+++ PostcodeValidator.Tests/PostcodeTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PostcodeValidator.Tests
+{
+    [TestClass]
+    public class PoscodeTests
+    {
+        [TestMethod]
+        public void IsValidPostCode_Junk_False()
+        {
+
+            //  arrange
+            bool expected = false;
+
+            //  act
+            bool testResult = Utilities.IsValidPostCode("");
+
+            //  assert
+            Assert.AreEqual(expected, testResult);
+        }
+
+        [TestMethod]
+        public void IsValidPostCode_Valid_True()
+        {
+
+            //  arrange
+            bool expected = true;
+
+            //  TODO:  create array of postcodes
+            //  TODO:  loop through each postcode and assert
+
+            //  act
+            bool testResult = Utilities.IsValidPostCode("LS29 6QH");
+
+            //  assert
+            Assert.AreEqual(expected, testResult);
+        }
+
+
+    }
+}
